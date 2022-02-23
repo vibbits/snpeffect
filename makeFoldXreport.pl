30120fc586d84d8a1a08a8fbdd30f5a71dbb641a
Alexander Botzki <alexander.botzki@vib.be>
2022-02-23 07:15:30 +0100

    add pLDDT to output of FoldXreport.tab

--- makeFoldXreport.pl
+++ makeFoldXreport.pl
@@ -6,6 +6,8 @@
 # interactingchains.tab. It writes as output FoldXreport.tab
 # (which will have as many records as variants4foldX.tab)
 
+$PDBFILEFOLDER = $ARGV[0];
+
 open IN, 'BuildModel_lastline.tab' or die "cannot open BuildModel_lastline.tab\n";
 while (<IN>) {
   chomp;
@@ -30,7 +32,7 @@ while (<IN>) {
 
 open IN, 'variants4foldX.tab' or die "cannot open variants4foldX.tab\n";
 open OUT, '>FoldXreport.tab';
-print OUT "variant\tchromosomal location\tmutation\tgene ID\tgene name\ttranscript ID\tlocation on transcript\tPDB file\tmutationstring\tchains\tinteracting chains\tdelta interaction energy\tSD\ttotal energy\tBackbone Hbond\tSidechain Hbond\tVan der Waals\tElectrostatics\tSolvation Polar\tSolvation Hydrophobic\tVan der Waals clashes\tentropy sidechain\tentropy mainchain\tsloop_entropy\tmloop_entropy\tcis_bond\ttorsional clash\tbackbone clash\thelix dipole\twater bridge\tdisulfide\telectrostatic kon\tpartial covalent bonds\tenergy Ionisation\tEntropy Complex\n";
+print OUT "variant\tchromosomal location\tmutation\tgene ID\tgene name\ttranscript ID\tlocation on transcript\tPDB file\tmutationstring\tchains\tinteracting chains\tdelta interaction energy\tSD\ttotal energy\tBackbone Hbond\tSidechain Hbond\tVan der Waals\tElectrostatics\tSolvation Polar\tSolvation Hydrophobic\tVan der Waals clashes\tentropy sidechain\tentropy mainchain\tsloop_entropy\tmloop_entropy\tcis_bond\ttorsional clash\tbackbone clash\thelix dipole\twater bridge\tdisulfide\telectrostatic kon\tpartial covalent bonds\tenergy Ionisation\tEntropy Complex\tpLDDT\n";
 while (<IN>) {
   chomp;
   if (not /^\d+\t\w+:\d+-\d+\t[A-Z]+\/[A-Z]+\t.+\t.+[\w\.]+\t\d+\t(.+.pdb)\t(.*)$/) {
@@ -38,6 +40,11 @@ while (<IN>) {
   } else {
     $PDBfile = $1;
     $mutationstring = $2;
+#    $PDBfilepath = "${PDBFILEFOLDER}/${PDBfile}";
+#    $aminoacid = $3;
+#    $chain = $4;
+#    $number = $5;
+#    $tempfactor = fetch_temp_factor($PDBfilepath, $chain, $number);
     $key = "${PDBfile}\t${mutationstring}";
     if (not exists $BuilModeloutput{$key}){
       die "pair $key not found in BuildModel_lastline.tab\n";
@@ -45,7 +52,38 @@ while (<IN>) {
     if (not exists $chainsinfo{$key}){
       die "pair $key not found in interactingchains.tab\n";
     }
-    print OUT "var_$_\t$chainsinfo{$key}\t$BuilModeloutput{$key}\n";
+    print OUT "var_$_\t$chainsinfo{$key}\t$BuilModeloutput{$key}\t${tempfactor}\n";
   }
 }
-close IN;
+close IN; close OUT;
+
+sub setflag {
+  # to indicate that script has terminated, notwithstanding it crashed or
+  #   # ended normally ; useful if script is started under a masterscript
+  open FLAG, '>flag.PDBTempFactor';
+  close FLAG;
+}
+  #
+sub fetch_temp_factor {
+  my ($PDBfile, $chain, $number) = @_;
+  #           # since we are dealing with structures from AlphaFold, we only take the last match and its temp factor
+  open INPDB, "${PDBfile}";
+  while (<INPDB>) {
+    chomp;
+    my $line = $_;
+   if ($line =~ m/^ATOM\s+\d+\s+\w+\s+(\w{3})\s+(\w)\s?(\s?\s?\d+)\s+(\S+\.\S+)\s+(\S+\.\S+)\s+(\S+\.\S+)\s+.+\..+\..+/ig) {
+      $id = $3;
+      $id =~ s/^\s+//;
+      if ($2 eq $chain & $id eq $number & length($line) == 66 ) {
+         $tempfactor = substr($line, -6);
+         $tempfactor =~ s/^\s+//;
+      }
+    }
+  }
+  if (not $tempfactor) {
+     &setflag;
+     die "Amino acid ${number} of ${chain} not found in ${PDBfile}\n";
+  }
+  close INPDB;
+  return $tempfactor;
+}
